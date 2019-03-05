90c00c5b769fa23e573fc0ac31782a0f6820669d
Jaeguins <jsu4838@naver.com>
2019-03-05 14:52:31 +0900

    add layout

--- app/src/main/java/com/slack/joiple/noti_2048/NotiBackService.java
+++ app/src/main/java/com/slack/joiple/noti_2048/NotiBackService.java
@@ -1,15 +1,43 @@
 package com.slack.joiple.noti_2048;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+import android.widget.RemoteViews;
 
 public class NotiBackService extends Service {
-
+    private static boolean Working=false;
+    Notification noti;
+    NotificationCompat.Builder notiBuilder;
+    RemoteViews.RemoteView baseView,expandedView;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Working=true;
+        notiBuilder=new NotificationCompat.Builder(this);
+        notiBuilder.setSmallIcon(R.drawable.ic_launcher_foreground);
+        notiBuilder.setContentTitle("Noti 2048");
+        notiBuilder.setContentText("swipe down to open");
+
+        notiBuilder.setStyle(new NotificationCompat.DecoratedCustomViewStyle());
+        notiBuilder.setCustomContentView(baseView);
+        notiBuilder.setCustomBigContentView(expandedView)
+
+        return START_NOT_STICKY;
+    }
+    @Override
+    public void onDestroy(){
+        Working=false;
+    }
+    public static boolean isWorking(){
+        return Working;
     }
 }
--- app/src/main/java/com/slack/joiple/noti_2048/OptionActivity.java
+++ app/src/main/java/com/slack/joiple/noti_2048/OptionActivity.java
@@ -1,15 +1,49 @@
 package com.slack.joiple.noti_2048;
 
+import android.app.ActivityManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class OptionActivity extends AppCompatActivity {
-    public Button StartButton;
+public class OptionActivity extends AppCompatActivity implements View.OnClickListener{
+    public Button startButton;
+    public TextView statusIndicator;
+    public NotiBackService service;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_option);
-        StartButton=findViewById(R.id.starter);
+
+        //Binding
+        startButton =findViewById(R.id.starter);
+
+        //EventBinding
+        startButton.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.starter:
+                startService(new Intent(getApplicationContext(),NotiBackService.class));
+                break;
+            default:
+                Log.w("EVENT","Unbinded Event");
+                break;
+        }
+    }
+    private boolean isServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)){
+            if("NotiBackService".equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
